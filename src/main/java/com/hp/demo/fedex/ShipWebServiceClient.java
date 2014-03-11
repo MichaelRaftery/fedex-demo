eb689512218ffd5b3c8a14fe304fa6cf8c1dae1e
alex_alm <alex_alm@ae2ec594-5fbe-9c4b-821e-43d3a997884e>
2014-03-11 11:29:07 +0000

    git-svn-id: https://ALM-DEV:8443/svn/hp-alm-demo/trunk/fedex-demo@55 ae2ec594-5fbe-9c4b-821e-43d3a997884e

--- src/main/java/com/hp/demo/fedex/ShipWebServiceClient.java
+++ src/main/java/com/hp/demo/fedex/ShipWebServiceClient.java
@@ -26,7 +26,7 @@ import java.util.Calendar;
 // fifth change
 //
 //Sample code to call the FedEx Ship Service - Domestic Express
-//test 11 march 2014
+//test 11 march 2014 new line of code 
 public class ShipWebServiceClient 
 {
     public static final String LABEL_LOCATION;
